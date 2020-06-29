34a189360c3cf894eacc57a3dc8596339fa806e1
Devesh Lohia <39876839+DeveshLohia@users.noreply.github.com>
2020-06-29 20:50:22 +0530

    Update sheet1.java

--- sheet1.java
+++ sheet1.java
@@ -1,1 +1,11 @@
-
+String
+	StringBuilder
+		1. Helps in making mutable String
+		2. Works better if size given in advance.
+			StringBuilder sb = new StringBuilder(40);
+		3. sb.capacity() -> 40
+		4. append (appends at the end)and insert (adds in between)
+			sb.append("I flew to ");
+			String location = "Delhi";
+			ab.append(location);
+			
